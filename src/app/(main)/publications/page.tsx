03cb2aeebe16de0fd29d43da86f10c0c37067ac1
dev-louis <louis@worldoflouis.co.uk>
2025-09-20 10:52:16 +0100

    added loading state to publications page

--- src/app/(main)/publications/page.tsx
+++ src/app/(main)/publications/page.tsx
@@ -1,7 +1,5 @@
 import PublicationsLayout from "@/components/publications/publications-layout";
-import { getPublications } from "@/lib/actions";
 
 export default async function Publications() {
-  const publications = await getPublications();
-  return <PublicationsLayout publications={publications} />;
+  return <PublicationsLayout />;
 }
--- src/components/publications/publications-layout.tsx
+++ src/components/publications/publications-layout.tsx
@@ -1,20 +1,41 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import { Publication } from "@/types";
 import PublicationsTable from "./publications-table";
+import { getPublications } from "@/lib/actions";
+import { Loader2Icon } from "lucide-react"; // Make sure this import is correct for your icon library
 
-type Props = {
-  publications: Publication[];
-};
+type Props = {};
 
 const PublicationsLayout = (props: Props) => {
+  const [publications, setPublications] = React.useState<Publication[]>([]);
+  const [loading, setLoading] = React.useState(true);
+
+  useEffect(() => {
+    const fetchPublications = async () => {
+      setLoading(true);
+      const publications = await getPublications();
+      setPublications(publications);
+      setLoading(false);
+    };
+
+    fetchPublications();
+  }, []);
+
   return (
     <div className="flex flex-col gap-4">
       <h1 className="text-2xl font-semibold">
-        {props.publications.length} Publications
+        {publications.length} Publications
       </h1>
-      <PublicationsTable publications={props.publications} />
+      {loading ? (
+        <div className="flex justify-center items-center py-10 flex-col gap-2">
+          <Loader2Icon className="h-8 w-8 text-primary animate-spin" />
+          <p className="text-sm text-muted-foreground">Loading...</p>
+        </div>
+      ) : (
+        <PublicationsTable publications={publications} />
+      )}
     </div>
   );
 };
