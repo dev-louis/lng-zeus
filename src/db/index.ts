3f1f6db8cfa0dce988d8106fb3ced6b0471ab134
dev-louis <louis@worldoflouis.co.uk>
2025-09-20 10:08:50 +0100

    potential fix for db connections

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,6 +3,10 @@ import { drizzle } from "drizzle-orm/node-postgres";
 
 import * as schema from "@/db/schema";
 
-export const db = drizzle(process.env.DATABASE_URL!, {
-  schema,
+export const db = drizzle({
+  connection: {
+    connectionString: process.env.DATABASE_URL!,
+    ssl: process.env.NODE_ENV === "production" ? true : false,
+  },
+  schema: schema,
 });
